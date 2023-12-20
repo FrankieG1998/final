dc0b1f6f3ca0bd01fb29c4ee527b9a64bb7eb424
FrankieG1998 <139153734+FrankieG1998@users.noreply.github.com>
2023-12-20 12:24:10 -0500

    Update server.ts

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,72 +1,18 @@
 let token = "196e1d0a5341fce18ebdd73f592772e3e85d91f1a2dd2582"
+import axios from "axios";
 
-export const server_calls ={
-    get: async () => {
-        const response = await fetch(`https://finalprojectback-dqrp.onrender.com/images`,
-        {
-            method: 'GET',
-            headers:{
-                'Content-Type': 'application/json', 
-                //'Access-Control-Allow-Origin': '*',
-                'x-access-token': `Bearer ${token}`
-            },
-            //mode: 'cors'
-        });
-        if (!response.ok){
-            throw new Error('Failed to fetch data');
-        }
-        return await response.json()
-    },
-
-    create: async(data: any = {}) =>{
-        const response = await fetch(`https://finalprojectback-dqrp.onrender.com/images`,
-        {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json', 
-                //'Access-Control-Allow-Origin': '*',
-                'x-access-token': `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
-            //mode: 'cors'
-        });
-        if (!response.ok){
-            throw new Error('Failed to create data');
-        }
-        return await response.json()
-    },
-
-    update: async(id:string, data: any = {}) =>{
-        const response = await fetch(`https://finalprojectback-dqrp.onrender.com/images/${id}`,
-        {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json', 
-                //'Access-Control-Allow-Origin': '*',
-                'x-access-token': `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
-            //mode: 'cors'
-        });
-        if (!response.ok){
-            throw new Error('Failed to update data');
-        }
-        return await response.json()
-    },
-
-    delete: async(id:string) =>{
-        const response = await fetch(`https://finalprojectback-dqrp.onrender.com/images/${id}`,
-        {
-            method: 'DELETE',
-            headers:{
-                'Content-Type': 'application/json', 
-                'x-access-token': `Bearer ${token}`
-            },
-            //mode: 'cors'
-        });
-        if (!response.ok){
-            throw new Error('Failed to delete data');
-        }
-        return;
-    },
-}
+export const options = {
+      method: 'GET',
+      url: 'https://f1-drivers-quotes.p.rapidapi.com/quotes',
+      headers: {
+        'X-RapidAPI-Key': '381890a8e7msha5241150939a6e4p175032jsn8da9e5a84293',
+        'X-RapidAPI-Host': 'f1-drivers-quotes.p.rapidapi.com'
+      }
+    };
+    
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
